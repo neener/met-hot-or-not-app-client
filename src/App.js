1b8f90f9210f783e9ee621d781899e2a0e09b588
Janine Rosen <janinemrosen@gmail.com>
2017-08-13 18:33:58 -0400

    deleting collection components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import Collections from './components/Collections';
 import MetObjects from './components/MetObjects'
-import CollectionService from './services/CollectionService';
 import MetObjectService from './services/MetObjectService';
 import './App.css';
 
@@ -16,20 +14,19 @@ class App extends Component {
   }
 
   componentDidMount() {
-    CollectionService.fetchCollections().then(collections => this.setState({ collections }))
     MetObjectService.fetchMetObjects().then(metobjects => this.setState({ metobjects }))
     
   }
 
   render() {
-    console.log(this.state.collections)
     return (
       <div className="App">
         <div className="navbar">
+          <h2> Metropolitan Museum of Art </h2>
           {/* <Navbar /> */}
         </div>
         <div className="sidebar">
-          <Collections collections={this.state.collections}/>
+          {/* <Departments /> */}
         </div>
         <div className="main-content">
           <MetObjects metobjects={this.state.metobjects} /> 
--- src/components/MetObjects.js
+++ src/components/MetObjects.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MetObjects = ({ metobjects }) => {
 	const renderMetObjects = metobjects.map(metobject =>
-		<p key={metobject.id}><img src ={metobject.image_url} /></p>
+		<p key={metobject.id}><img src ={metobject.image_url} alt="metobject url" /></p>
 	)
 
 	return (
