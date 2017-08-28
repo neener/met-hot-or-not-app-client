8e7b6fe91d4c463264492d5880b025917572f221
Janine Rosen <janinemrosen@gmail.com>
2017-08-28 15:56:28 -0400

    adding redux promise middleware

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,12 +1,16 @@
 import {
   createStore,
-  applyMiddleware,
-  combineReducers
+  applyMiddleware
 } from 'redux'
 import thunk from 'redux-thunk'
-import { createStore } from 'redux'
+import { createLogger } from 'redux-logger'
+import promiseMiddleware from 'redux-promise-middleware'
+import allReducers from './reducers/index'
 
 const store = createStore(
   allReducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(promiseMiddleware(), thunk, createLogger())
 )
+
+export default store
