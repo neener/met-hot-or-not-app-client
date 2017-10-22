07d2bf75b09b2ff89f7dd80823f6957d41c42ab8
Janine Rosen <janinemrosen@gmail.com>
2017-10-22 14:38:00 -0400

    images are displaying

--- src/components/MetObjects.js
+++ src/components/MetObjects.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const MetObjects = ({ metobjects }) => {
-	const renderMetObjects = (metobject =>
+	const renderMetObjects = metobjects.map(metobject =>
 		<p key={metobject.id}><img src={metobject.image_url} alt="metobject url" /></p>
 	)
 	return (
--- src/containers/App.js
+++ src/containers/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     super()
 
     this.state = {
-      metobjects: {}
+      metobjects: []
     }
   }
 
